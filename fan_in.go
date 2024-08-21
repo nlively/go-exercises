44f1286ce5b06818a43b23e06702eddc2ae93f07
Noah Lively <nlively@justworks.com>
2024-08-20 19:45:54 -0500

    applied fan-in fixes from chatgpt

--- fan_in.go
+++ fan_in.go
@@ -24,7 +24,6 @@ func upstream_sender(ch chan<- string, stream_num int, wg *sync.WaitGroup) {
 		ch <- fmt.Sprintf("Stream %d.%d", stream_num, i)
 	}
 
-	//close(ch)
 	fmt.Println("Done with stream", stream_num)
 }
 
@@ -37,8 +36,8 @@ func aggregator(agg_ch chan<- string, upstream_ch <-chan string, wg *sync.WaitGr
 }
 
 func final_receiver(ch <-chan string, done chan<- bool) {
-	for i := range ch {
-		fmt.Println("received message", i)
+	for msg := range ch {
+		fmt.Println("received message", msg)
 	}
 	done <- true
 }
@@ -55,32 +54,37 @@ func main() {
 	agg_ch := make(chan string)
 	done := make(chan bool)
 
-	wg.Add(1)
+	wg.Add(4) // Adjust this to match the number of upstream_senders
 	go upstream_sender(ch1, 1, &wg)
-	wg.Add(1)
 	go upstream_sender(ch2, 2, &wg)
-	wg.Add(1)
 	go upstream_sender(ch3, 3, &wg)
-	wg.Add(1)
 	go upstream_sender(ch4, 4, &wg)
-	wg.Wait()
 
-	close(ch1)
-	close(ch2)
-	close(ch3)
-	close(ch4)
+	// Close the channels after all upstream_senders are done
+	go func() {
+		wg.Wait()
+		close(ch1)
+		close(ch2)
+		close(ch3)
+		close(ch4)
+	}()
 
+	// Aggregators
 	agg_wg.Add(4)
 	go aggregator(agg_ch, ch1, &agg_wg)
 	go aggregator(agg_ch, ch2, &agg_wg)
 	go aggregator(agg_ch, ch3, &agg_wg)
 	go aggregator(agg_ch, ch4, &agg_wg)
-	agg_wg.Wait()
 
-	close(agg_ch)
+	// Close aggregator channel after all messages are processed
+	go func() {
+		agg_wg.Wait()
+		close(agg_ch)
+	}()
 
+	// Final receiver
 	go final_receiver(agg_ch, done)
 
-	<-done
+	<-done // Wait for final_receiver to finish
 	fmt.Println("Finished")
 }
